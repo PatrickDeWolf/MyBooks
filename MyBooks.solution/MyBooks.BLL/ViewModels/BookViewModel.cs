226b5343bd2456209e9e91a1a4e67fc2b0fd80bf
The Gooftroop <thegooftroop@hotmail.be>
2020-05-18 21:37:58 +0200

    Toezvoegen Book

---
+++ MyBooks.solution/MyBooks.BLL/ViewModels/BookViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+
+namespace MyBooks.BLL
+{
+	public class BookViewModel:ViewModelBase
+	{
+
+		// Boek
+		// lijsten van Genre, Publisher, Author
+
+		// Opdracht Save, 
+
+	}// end BookViewModel
+
+}
